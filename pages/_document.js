e49d7a66cb2a7d6c008f66fa793934ee9fcef63e
Darshan S <hitmandarshan34@gmail.com>
2024-05-21 07:15:21 +0530

    10th cammit

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,10 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="icon" href="/icon.png"></link>
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        ></meta>
       </Head>
       <body>
         <Main />
